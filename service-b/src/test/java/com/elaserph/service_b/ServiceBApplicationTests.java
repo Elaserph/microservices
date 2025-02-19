442245e4b1a039eaceee9968caeae0db3a3404b2
elaserph <elaserph.dev@gmail.com>
2025-02-19 17:54:17 +0530

    Genesis: Build Microservices components: Eureka Service Discovery and Registry, Service-A and Service-B

---
+++ service-b/src/test/java/com/elaserph/service_b/ServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elaserph.service_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
